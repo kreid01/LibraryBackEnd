3dbc9309c9aec9d1cef0df6e6a3d961133452d92
kreid01 <kieranreid2014@icloud.com>
2022-11-08 15:02:52 +0000

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,29 @@
+﻿using System.Data.SqlTypes;
+
+namespace LibrayBackEnd
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string StockNumber { get; set; }
+
+        public string Quality { get; set; }
+
+        public string Author { get; set; }
+
+        public int Pages { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Genre { get; set; }
+
+        public string Summary { get; set; }
+        
+        public string Published { get; set; }
+    }
+
+}
+    
---
+++ Controllers/BookController.cs
@@ -0,0 +1,84 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace LibrayBackEnd.Controllers
+{
+    public class BookController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public BookController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+        [Route("books")]
+        public async Task<ActionResult<List<Book>>> GetAllBooks()
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+
+            IEnumerable<Book> books = await SelectAllBooks(connection);
+
+            return Ok(books);
+        }
+
+      
+
+        [HttpGet]
+        [Route("books/{id}")]
+        public async Task<ActionResult<Book>> GetBook(int bookId)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+
+            var book = await connection.QueryFirstAsync<Book>("select * from books where id = @Id",
+                new { Id = bookId});
+
+            return Ok(book);
+        }
+
+
+        [HttpPost]
+        [Route("books")]
+        public async Task<ActionResult<Book>> CreateBook(Book book)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+
+            await connection.ExecuteAsync("insert into books (title, stockNumber, author, quality, pages, genre, summary, published, price) " +
+                "values (@Title, @StockNumber, @Author, @Quality, @Pages, @Genre, @Summary, @Published, @Price)", book);
+
+            return Ok(await SelectAllBooks(connection));
+        }
+
+        [HttpPut]
+        [Route("books")]
+        public async Task<ActionResult<Book>> UpdateBook(Book book)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+
+            await connection.ExecuteAsync("update books set title = @Title, stockNumber = @StockNumber, author = @Author, quality = @Quality, pages = @Pages, genre = @Genre, summary =@Summary, " +
+                "published = @Published, price = @Price where id = @Id", book);
+
+            return Ok(await SelectAllBooks(connection));
+        }
+
+        [HttpDelete]
+        [Route("books/{id}")]
+        public async Task<ActionResult<Book>> DeleteBook(int bookId)
+        {
+            using var connection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+
+            await connection.ExecuteAsync("delete from books where id = @Id", new { Id = bookId });
+            return Ok(await SelectAllBooks(connection));
+        }
+
+
+        private static async Task<IEnumerable<Book>> SelectAllBooks(SqlConnection connection)
+        {
+            return await connection.QueryAsync<Book>("select * from books");
+        }
+
+
+    }
+}
